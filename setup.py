747eb9adb01c799c11345489258dd6d75cecee33
arthur-lenoble <arthur.lenoble.22@gmail.com>
2025-04-15 12:15:34 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SnooPY",
+    version="1.0.2",
+    description="SnooPy is a python module made to learn easily the python syntax and access new commands"",
+    author="Arthur Lenoble",
+    author_email="artlen2207@outlook.fr",
+    packages=find_packages(),
+    install_requires=[],
+)
